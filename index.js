9e23b195688ff9b6f2f0b5c5fd70d229d2bbdb16
Faiza Javed <faizafj@outlook.com>
2023-09-12 22:46:21 +0100

    Finished main functionality

--- index.js
+++ index.js
@@ -32,43 +32,50 @@ const allQuestions = [
         userAnswer: " "
     }
 ]
-
+points = localStorage.setItem("points", "0") //set the points to 0, and add to local storage
 totalAnswered = localStorage.setItem("totalAnswered", "1") //sets the number of total answered questions to 0
 
-
+function addPoints(questions) { //adds points to the currentPoints to see how many the user has gotten correct.
+    currentPoints = localStorage.getItem("points")
+    if (currentPoints <= questions.length) {  //if points is less than the length of questions 
+        pointsValue = parseInt(currentPoints) + 1
+        localStorage.setItem("points", pointsValue)
+        currentPoints = localStorage.getItem("points")
+    }
+}
 
 function totalAnsweredCounter(questions) { //adds 1 to the number of questions the user has answered 
     counter = localStorage.getItem("totalAnswered")
-    console.log("1   " + counter)
     if (counter < questions.length) {  //if questions answered is less than the amount of questions 
         counterValue = parseInt(counter) + 1
         localStorage.setItem("totalAnswered", counterValue)
         counter = localStorage.getItem("totalAnswered") //check
-        console.log("counter = " + counter)
+        // console.log("counter = " + counter)
     }
     else {
-    console.log("counter " + counter + "questions.length " + questions.length)
+        console.log("counter " + counter + "questions.length " + questions.length)
+        quizEnd(counter, questions)
+    }
+}
 
-    quizEnd(counter, questions)
-}}
 
 
-
-//check if the users answers are correct ✅
+//check if the users answers are correct 
 function checkUserAnswer(questionsObj, questionId, userAnswer) {
     for (i = 0; i < questionsObj.length; i++) {
         if (questionsObj[i].questionId === questionId) { //checks for the questionId to know which question to reference
-            totalAnsweredCounter(questionsObj) //+1 to total answered questions
             if (userAnswer === questionsObj[i].correctAnswer) { //checks users answer against the correct answer
                 questionsContainer = document.getElementById('questionContainer' + questionId)
                 correctHeader = document.getElementById('correctHeader' + questionId)
                 correctHeader.innerText = ("Correct")
+                addPoints(questionsObj)
+                totalAnsweredCounter(questionsObj) //+1 to total answered questions
                 // return ("Yes")
             } else {
                 questionsContainer = document.getElementById('questionContainer' + questionId)
                 correctHeader = document.getElementById('correctHeader' + questionId)
                 correctHeader.innerText = ("Incorrect")
-
+                totalAnsweredCounter(questionsObj) //+1 to total answered questions
                 // return ("Nope!")
             }
         }
@@ -84,10 +91,9 @@ const getUserAnswer = (e) => { //function which gets the inner text value from a
     console.log(userAnswer)
     result = checkUserAnswer(allQuestions, questionId, userAnswer)
     // console.log(result)
-    // displayAnswerResult(result, questionId)
 }
 
-function displayQuestionAndAnswers(questionsObj) { //✅
+function displayQuestionAndAnswers(questionsObj) { //
     mainQuestionsContainer = document.getElementById('mainQuestionsContainer')
     for (i = 0; i < questionsObj.length; i++) {
         questionsContainer = document.createElement('article')
@@ -122,22 +128,23 @@ function quizEnd(questionsAnswered, questionsObj) {
     totalCorrectText = document.getElementById('totalCorrect')
     totalIncorrectText = document.getElementById('totalIncorrect')
 
-    totalCorrectText.innerText = "Hello"
-
     numberOfQuestions = questionsObj.length
     questionsAnswered = parseInt(questionsAnswered)
+    totalPoints = localStorage.getItem("points")
 
     console.log("noQuestions " + numberOfQuestions)
-
+    console.log("totalPoints " + totalPoints)
     console.log("questionsAnswered " + questionsAnswered)
 
-    if (questionsAnswered === numberOfQuestions) {
-        console.log("This should work")
-        totalCorrectText.innerText = ("You got 5"  + " right!")
-    } else {
-        console.log("This doesnt work")
-        totalCorrectText.innerText = "This doesnt work"
-    }
+    if (questionsAnswered === numberOfQuestions) { //If all questions were answered
+        if (parseInt(totalPoints) === numberOfQuestions) {
+            totalCorrectText.innerText = ("Congrats! You got all" + parseInt(totalPoints) + " questions right!")
+        } else {
+            totalCorrectText.innerText = ("You got " + parseInt(totalPoints) + " questions right!")
+            totalIncorrectText.innerText = ("And you got " + (numberOfQuestions - parseInt(totalPoints)) + " wrong!")
+        }
+
+    } 
 }
 displayQuestionAndAnswers(allQuestions)
 
